480bab6ca5aefa3ae3b1a9c19b48b76bcc7c15d8
lusiyang <lusiyanga@enn.cn>
2018-04-11 21:52:37 +0800

    jedis单机版集群版配置在spring的xml中，测试冲配置文件中读取jedis配置读取redis缓存

--- taotao-rest/src/test/java/com/taotao/rest/jedis/JedisTest.java
+++ taotao-rest/src/test/java/com/taotao/rest/jedis/JedisTest.java
@@ -3,6 +3,8 @@ package com.taotao.rest.jedis;
 import java.util.HashSet;
 
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.Jedis;
@@ -69,4 +71,43 @@ public class JedisTest {
 		//关闭集群jedis
 		jedisCluster.close();
 	}
+	
+	/*
+	 * 从xml配置文件(spring配置)取jedis和jedis集群--单机版整合
+	 */
+	@Test
+	public void testJedisByXml() {
+		//读取spring的配置文件applicationContext
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring/applicationContext-*.xml");
+		//从配置文件获取对象jedisPool
+		JedisPool jedisPool = (JedisPool) applicationContext.getBean("jedisClient");
+		//从连接池jedisPool对象中取出jedis对象
+		Jedis jedis = jedisPool.getResource();
+		//设置jedis参数
+		jedis.set("key3", "2000");
+		String string = jedis.get("key3");
+		System.out.println(string);
+		//关闭jedis
+		jedis.close();
+		//关闭pool连接池
+		jedisPool.close();
+	}
+	
+	/*
+	 * 从xml配置文件(spring配置)取jedis和jedis集群--集群版
+	 */
+	@Test
+	public void testJedisClusterByXml() {
+		//读取spring的配置文件applicationContext
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring/applicationContext-*.xml");
+		//从配置文件获取对象jedisCluster
+		JedisCluster jedisCluster = (JedisCluster) applicationContext.getBean("jedisClient");
+		//设置集群jedis参数
+		jedisCluster.set("key4", "4000");
+		String string = jedisCluster.get("key4");
+		System.out.println(string);
+		//关闭集群jedis
+		jedisCluster.close();
+	}
+
 }
