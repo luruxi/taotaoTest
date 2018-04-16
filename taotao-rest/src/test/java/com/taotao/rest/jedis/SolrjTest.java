1fc852bcc069d668c2fcc7984d7eac0d2934fa99
lusiyang <lusiyanga@enn.cn>
2018-04-16 21:42:53 +0800

    测试通过查询条件查询solr的文档内容

--- taotao-rest/src/test/java/com/taotao/rest/jedis/SolrjTest.java
+++ taotao-rest/src/test/java/com/taotao/rest/jedis/SolrjTest.java
@@ -4,9 +4,14 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrResponse;
 import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
 import org.apache.solr.common.SolrInputDocument;
 import org.junit.Test;
 
@@ -45,5 +50,25 @@ public class SolrjTest {
 //		solrServer.deleteByQuery("*:*");
 		//提交
 		solrServer.commit();
+	}
+	/*
+	 * 通过查询条件查询solr
+	 */
+	@Test
+	public void queryDocument() throws Exception {
+		//创建连接
+		SolrServer solrServer = new HttpSolrServer("http://192.168.159.130:8080/solr/");
+		//新建查询对象
+		SolrQuery query = new SolrQuery();
+		//设置查询条件
+		query.setQuery("*:*");
+		//执行查询
+		QueryResponse response = solrServer.query(query);
+		//从查询结果里取数据
+		SolrDocumentList results = response.getResults();
+		for (SolrDocument solrDocument : results) {
+			System.out.println(solrDocument.get("id"));
+			System.out.println(solrDocument.get("item_title"));
+		}
 	}
 }
