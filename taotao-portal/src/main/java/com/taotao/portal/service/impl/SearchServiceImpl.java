9f0f911c65a0d48e01028d19ba7cf2e53f982037
lusiyang <lusiyanga@enn.cn>
2018-04-17 22:05:37 +0800

    门户页面搜索功能调用taotao-search服务的搜索接口，处理处理返回数据，返回到jsp上显示

---
+++ taotao-portal/src/main/java/com/taotao/portal/service/impl/SearchServiceImpl.java
@@ -0,0 +1,44 @@
+package com.taotao.portal.service.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.common.utils.HttpClientUtil;
+import com.taotao.portal.pojo.SearchResult;
+import com.taotao.portal.service.SearchService;
+/*
+ * 商品搜索service
+ */
+@Service
+public class SearchServiceImpl implements SearchService {
+	@Value("${SEARCH_BASE_URL}")
+	private String SEARCH_BASE_URL;
+	@Override
+	public SearchResult search(String queryString, int page) {
+		//调用taotao-search的服务器
+		//查询参数
+		Map<String,String> param = new HashMap<>();
+		param.put("q",queryString);
+		param.put("page", page+"");
+		try {
+			//调用服务
+			String json = HttpClientUtil.doGet(SEARCH_BASE_URL,param);
+			//把字符串转换成java对象
+			//List<SearchResult> list = JsonUtils.jsonToList(json, SearchResult.class);
+			TaotaoResult taotaoResult = TaotaoResult.formatToPojo(json, SearchResult.class);
+			if(taotaoResult.getStatus()==200) {
+				SearchResult result = (SearchResult) taotaoResult.getData();
+				return result;
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
